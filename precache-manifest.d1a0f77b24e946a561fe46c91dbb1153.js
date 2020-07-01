ed69035da3c160775329831b4802640a4edae62f
jeremy wambugu <wambugujeremy@gmail.com>
2020-07-01 20:05:35 +0300

    Updates

---
+++ precache-manifest.d1a0f77b24e946a561fe46c91dbb1153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f452a992dd1fef00e50143ff3738c225",
+    "url": "/JeremyWambugu/Corona-tracker-react.git/index.html"
+  },
+  {
+    "revision": "a92a782b824a0445683c",
+    "url": "/JeremyWambugu/Corona-tracker-react.git/static/css/main.c66d0d4c.chunk.css"
+  },
+  {
+    "revision": "d58f045cb8eb542e0f09",
+    "url": "/JeremyWambugu/Corona-tracker-react.git/static/js/2.764dd5e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JeremyWambugu/Corona-tracker-react.git/static/js/2.764dd5e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92a782b824a0445683c",
+    "url": "/JeremyWambugu/Corona-tracker-react.git/static/js/main.7274dd9b.chunk.js"
+  },
+  {
+    "revision": "a06e982ac0216e31c6dc",
+    "url": "/JeremyWambugu/Corona-tracker-react.git/static/js/runtime-main.06c0f666.js"
+  }
+]);
